5c5c940651e919d265038af64dc10e5b36fcbd14
rakss17 <bohariambulo17@gmail.com>
2026-04-17 20:16:10 +0800

    fix migration files and db context

--- Models/User.cs
+++ Models/User.cs
@@ -8,17 +8,6 @@ namespace Sadkah.Backend.Models
 {
     public class User : IdentityUser
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-        public UserRole Role { get; set; }
-        public bool IsVerified { get; set; } = false;
-        public List<Campaign> Campaigns { get; set; } = new();
-        public List<Donation> Donations { get; set; } = new();
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? DeletedAt { get; set; }
+
     }
 }
