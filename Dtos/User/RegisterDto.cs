8e9efc4dc4a6f4a402cf61340f9a4d379d6f3740
rakss17 <bohariambulo17@gmail.com>
2026-04-24 19:40:22 +0800

    fixed register dto password validation

--- Dtos/User/RegisterDto.cs
+++ Dtos/User/RegisterDto.cs
@@ -12,7 +12,9 @@ namespace Sadkah.Backend.Dtos.User
         [EmailAddress]
         public string? Email { get; set; } = string.Empty;
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Passwords must be at least 8 characters.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d).+$",
+            ErrorMessage = "Passwords must have at least one digit ('0'-'9') and one uppercase ('A'-'Z').")]
         public string Password { get; set; } = string.Empty;
         [Required]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
